fcadceeca589c3fbba15d9f8b14085bb2516b2a2
Deepanshu Kumar <deepskmr822@gmail.com>
2025-12-11 11:17:36 +0530

    add express

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,7 @@ export default defineConfig(
             },
         },
         rules: {
-            'no-console': 'error',
+            // 'no-console': 'error',s
         },
     },
 );
---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import app from './app';
+import { Config } from './config';
+
+const startServer = () => {
+    const PORT = Config.PORT;
+    try {
+        app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+startServer();
